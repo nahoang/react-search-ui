12e422340b4205e6ad5c9a7dc96da34d5664e491
nahoang <nahoang9698@gmail.com>
2021-01-15 23:36:26 +0700

    style(layout): make the section specific

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import LayoutSidebar from './LayoutSidebar';
+import LayoutSidebar from "./LayoutSidebar";
 
 const Layout = (props) => {
   const { children, sideContent } = props;
   return (
     <div className="layout">
       <div className="layout-header">
-
+        Header
       </div>
       <div className="layout-body">
         <div className="layout-body__inner">
           <LayoutSidebar className="layout-sidebar">
-            {sideContent}
+            Sidebar here!
           </LayoutSidebar>
-        {children}
+          <div className="layout-main">
+            <div className="layout-main-header">
+              <div className="layout-main-header__inner">Search Box</div>
+            </div>
+            <div className="layout-main-body">
+              {children}
+            </div>
+            <div className="layout-main-footer">
+              Footer
+            </div>
+          </div>
         </div>
        
       </div>
